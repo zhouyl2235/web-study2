65ccd5bbca1e4a883f94c70ec2da6c3e105664de
zylpc <zhouyunlong1101@gmail.com>
2024-12-06 12:11:46 +0800

    项目创建

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="layout">
+<!--    左侧导航区域-->
+    <MymMnu class="menu"/>
+<!--      右侧内容区域-->
+    <Content class="content"/>
+  </div>
+</template>
+
+<script>
+import MymMnu from './Mymenu.vue'
+import Content from './Content.vue'
+
+export default {
+  components: {
+    MymMnu,
+    Content
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.layout{
+  display: flex;
+  .menu{
+    width: 200px;
+    min-height: 500px;
+    background: #666;
+    top:0;
+
+  }
+  .content{
+    margin-left: 200px;
+  }
+}
+
+</style>
